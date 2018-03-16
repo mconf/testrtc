5479519c6e6ae109f237c9ae38b481ef51ce1688
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2018-03-16 10:41:20 -0300

    Cleaning up server.js

--- server.js
+++ server.js
@@ -4,18 +4,6 @@ const app = express();
 
 app.use('/testrtc', express.static(path.join(__dirname, 'out/src')));
 
-app.get('/', function(req, res) {
-  res.sendFile('index.html');
-})
-
-app.get('/main.js', function(req, res) {
-  res.sendFile('main.js');
-});
-
-app.get('/favicon.ico', function(req, res) {
-  res.sendFile('images/favicon.ico');
-});
-
 app.listen(3010, function() {
   console.log('Live at Port 3010');
 });
